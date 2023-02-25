ba259e409293ad8ea6829a5785ced2567bd960d5
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-02-25 09:34:41 +0500

    asset image

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 
-
-void main(){
-  runApp(
-    MaterialApp(home: MyApp(),)
-  );
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
-
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -18,14 +16,15 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxHU2pIfAAt5C41lgLMYk3C9fln0ePEqJvDg&usqp=CAU'),
-          Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSWrlhRJhkGvL5U05n6x07mbZ5bKuVvZCcsw&usqp=CAU'),
-          Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_-xxyqGwUsc7v_6T7oc1zidQBu_G8t9W02w&usqp=CAU'),
-          Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1LLmBSwlEHJ3S1X1Y4aB2fShVVg5Urr4wXw&usqp=CAU'),
-
+          Image.asset(
+            'images/tecno.jpg',
+            fit: BoxFit.fill,
+           
+          ),
         ],
       ),
     );
